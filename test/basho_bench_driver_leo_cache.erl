a1b3fdc740da1bf5cd4004896d8c63b93bfc3184
Yosuke Hara <yousuke.hara@gmail.com>
2013-04-12 22:18:03 +0900

    Add benchmark suit

---
+++ test/basho_bench_driver_leo_cache.erl
@@ -0,0 +1,58 @@
+%% -------------------------------------------------------------------
+%%
+%% Leo Cache - Benchmarking Suite
+%%
+%% Copyright (c) 2012 Rakuten, Inc.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+-module(basho_bench_driver_leo_cache).
+-author("Yosuke Hara").
+
+-include("leo_cache.hrl").
+
+-export([new/1,
+         run/4]).
+
+%% ====================================================================
+%% API
+%% ====================================================================
+
+new(_Id) ->
+    leo_cache_api:start(?DEF_OPTIONS),
+    {ok, null}.
+
+
+run(get, KeyGen, _ValueGen, State) ->
+    BinKey = list_to_binary(KeyGen()),
+    case leo_cache_api:get(BinKey) of
+        {ok, _Value} ->
+            {ok, State};
+        not_found ->
+            {ok, State};
+        {error, Reason} ->
+            {error, Reason}
+    end;
+
+run(put, KeyGen, ValueGen, State) ->
+    BinKey = list_to_binary(KeyGen()),
+    case leo_cache_api:put(BinKey, ValueGen()) of
+        ok ->
+            {ok, State};
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
